a97c18ca3be18ffd72e7dbc29ea7db3dde23da9d
inatoihs <sabanna.taihei@gmail.com>
2023-10-21 14:45:08 +0900

    データの値によってピクセルの大きさを変更するようにしました

--- region_visualizer.py
+++ region_visualizer.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.patches import Polygon
 
 
@@ -19,42 +20,53 @@ def visualizer(
     # グラフのサイズを設定
     plt.figure(figsize=(5, 5))
 
-    # カラーマップを選択（ここでは'viridis'を使用）
-    cmap = plt.get_cmap("viridis")
+    xticks_placement = [-0.5]
+    for i in range(x_max):
+        xticks_placement.append(i + 0.5)
+    yticks_placement = [-0.5]
+    for i in range(y_max):
+        yticks_placement.append(i + 0.5)
 
-    # データをimshowを使用して描画
-    plt.imshow(data, cmap=cmap, origin="lower", interpolation="nearest")
+    # x軸とy軸の目盛りを設定
+    plt.xticks(xbins[::2], xbins[::2])
+    plt.yticks(ybins[::2], ybins[::2])
+    plt.xticks(rotation=50)
+
+    X, Y = np.meshgrid(xbins, ybins)
+
+    plt.pcolormesh(X, Y, data)
 
     # 領域の線を描画するための座標を定義
     region_coords = []
 
-    shift_width = 0.0035 * x_max  # 領域の端っこ線をずらすための値
+    x_shift_width = 0.01 * x_max  # 領域の端っこ線をずらすための値
+    y_shift_width = 0.01 * y_max  # 領域の端っこ線をずらすための値
     for i in range(left_index, right_index + 1):
-        region_coords.append([i - 0.5, bot[i] - 0.5])
-        region_coords.append([i + 0.5, bot[i] - 0.5])
+        region_coords.append([xbins[i], ybins[bot[i]]])
+        region_coords.append([xbins[i + 1], ybins[bot[i]]])
         if i == 0:
-            region_coords[-2][0] += shift_width
+            region_coords[-2][0] += x_shift_width
         elif i == x_max - 1:
-            region_coords[-1][0] -= shift_width
+            region_coords[-1][0] -= x_shift_width
         if bot[i] == 0:
-            region_coords[-2][1] += shift_width
-            region_coords[-1][1] += shift_width
+            region_coords[-2][1] += y_shift_width
+            region_coords[-1][1] += y_shift_width
         elif bot[i] == y_max - 1:
-            region_coords[-2][1] -= shift_width
-            region_coords[-1][1] -= shift_width
+            region_coords[-2][1] -= y_shift_width
+            region_coords[-1][1] -= y_shift_width
     for i in range(right_index, left_index - 1, -1):
-        region_coords.append([i + 0.5, top[i] + 0.5])
-        region_coords.append([i - 0.5, top[i] + 0.5])
+        region_coords.append([xbins[i + 1], ybins[top[i]]])
+        region_coords.append([xbins[i], ybins[top[i]]])
         if i == 0:
-            region_coords[-1][0] += shift_width
+            region_coords[-1][0] += x_shift_width
         elif i == x_max - 1:
-            region_coords[-2][0] -= shift_width
+            region_coords[-2][0] -= x_shift_width
         if top[i] == 0:
-            region_coords[-2][1] += shift_width
-            region_coords[-1][1] += shift_width
+            region_coords[-2][1] += y_shift_width
+            region_coords[-1][1] += y_shift_width
         elif top[i] == y_max - 1:
-            region_coords[-2][1] -= shift_width
-            region_coords[-1][1] -= shift_width
+            region_coords[-2][1] -= y_shift_width
+            region_coords[-1][1] -= y_shift_width
 
     # Polygonオブジェクトを作成
     region_polygon = Polygon(
@@ -62,7 +74,7 @@ def visualizer(
         closed=True,
         edgecolor="red",
         facecolor="none",
-        linewidth=3,
+        linewidth=2,
     )
 
     # グラフにPolygonを追加
@@ -74,16 +86,6 @@ def visualizer(
     # x軸とy軸のラベルを設定
     plt.xlabel(column_x)
     plt.ylabel(column_y)
-
-    xticks_placement = [-0.5]
-    for i in range(x_max):
-        xticks_placement.append(i + 0.5)
-    yticks_placement = [-0.5]
-    for i in range(y_max):
-        yticks_placement.append(i + 0.5)
-
-    plt.xticks(xticks_placement[::2], xbins[::2])
-    plt.yticks(yticks_placement[::2], ybins[::2])
 
     # グラフを表示
     plt.show()
